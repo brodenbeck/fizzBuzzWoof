14696fb126fe0b120ec69a1c8f0a3d085f83f0b0
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2016-04-26 17:09:25 -0400

    added whitespace for readability

--- source.js
+++ source.js
@@ -1,37 +1,47 @@
 function fizzBuzzWoof(i) {
   var string = i + ""; 
-  if(divisibleBy(i, 7) || string.includes("7")) {
-    if(divisibleBy(i, 5) || string.includes("5")) {
-      if(divisibleBy(i, 3) || string.includes("3")) {
-        return "fizzbuzzwoof";
+    
+    if(divisibleBy(i, 7) || string.includes("7")) {
+      if(divisibleBy(i, 5) || string.includes("5")) {
+        if(divisibleBy(i, 3) || string.includes("3")) {
+          return "fizzbuzzwoof";
+        }
       }
     }
-  }
-  if(divisibleBy(i, 7) || string.includes("7")) {
-    if(divisibleBy(i, 5) || string.includes("5")) {
-      return "buzzwoof";
+    
+    if(divisibleBy(i, 7) || string.includes("7")) {
+      if(divisibleBy(i, 5) || string.includes("5")) {
+        return "buzzwoof";
+      }
     }
-  }
-  if(divisibleBy(i, 3) || string.includes("3")) {
-    if(divisibleBy(i, 5) || string.includes("5")) {
-      return "fizzbuzz";
-    }
-  }
-  if(divisibleBy(i, 7) || string.includes("7")) {
+    
     if(divisibleBy(i, 3) || string.includes("3")) {
-      return "fizzwoof";
+      if(divisibleBy(i, 5) || string.includes("5")) {
+        return "fizzbuzz";
+      }
     }
+    
+    if(divisibleBy(i, 7) || string.includes("7")) {
+      if(divisibleBy(i, 3) || string.includes("3")) {
+        return "fizzwoof";
+      }
+    }
+    
+    if(divisibleBy(i, 3) || string.includes("3")) {
+      return "fizz";
+    }
+    
+    if(divisibleBy(i, 5) || string.includes("5")) {
+      return "buzz";
+    }
+    
+    if(divisibleBy(i, 7) || string.includes("7")) {
+      return "woof";
+    }
+    
+    return i;
+    
   }
-  if(divisibleBy(i, 3) || string.includes("3")) {
-    return "fizz";
-  }
-  if(divisibleBy(i, 5) || string.includes("5")) {
-    return "buzz";
-  }
-  if(divisibleBy(i, 7) || string.includes("7")) {
-    return "woof";
-  }
-  return i;
 }
 
 function divisibleBy(num, divisor) {
